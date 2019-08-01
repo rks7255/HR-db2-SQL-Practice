6ed2a695a61bc5cecb97f7bb15192115897d1533
Raj Shah <rshah7255@gmail.com>
2019-08-01 10:38:03 -0400

    Update HRQueries.sql

--- SQL/HRQueries.sql
+++ SQL/HRQueries.sql
@@ -14,8 +14,7 @@ SELECT F_NAME, L_NAME
 FROM EMPLOYEES
 WHERE B_DATE LIKE '197%';
 
--- Query 3: Retrieve all employees in department 5 whose salary is between 
--- 60000 and 70000.
+-- Query 3: Retrieve all employees in department 5 whose salary is between 60000 and 70000.
 
 SELECT *
 FROM EMPLOYEES
@@ -27,8 +26,7 @@ SELECT F_NAME, L_NAME, DEP_ID
 FROM EMPLOYEES
 ORDER BY DEP_ID;
 
--- Query 4B: Retrieve a list of employees ordered in descending order by 
--- department ID and within each department ordered alphabetically in 
+-- Query 4B: Retrieve a list of employees ordered in descending order by department ID and within each department ordered alphabetically in 
 -- descending order by last name.
 
 SELECT F_NAME, L_NAME, DEP_ID
@@ -41,15 +39,13 @@ SELECT DEP_ID, COUNT(*)
 FROM EMPLOYEES
 GROUP BY DEP_ID;
 
--- Query 5B: For each department retrieve the number of employees in the
--- department, and the average employees salary in the department.
+-- Query 5B: For each department retrieve the number of employees in the department, and the average employees salary in the department.
 
 SELECT DEP_ID, COUNT(*), AVG(SALARY)
 FROM EMPLOYEES
 GROUP BY DEP_ID;
 
--- Query 5C: Label the computed columns in the result set of Query 5B as
--- “NUM_EMPLOYEES” and “AVG_SALARY”.
+-- Query 5C: Label the computed columns in the result set of Query 5B as “NUM_EMPLOYEES” and “AVG_SALARY”.
 
 SELECT DEP_ID, COUNT(*) AS "NUM_EMPLOYEES", AVG(SALARY) AS "AVG_SALARY"
 FROM EMPLOYEES
@@ -70,8 +66,7 @@ GROUP BY DEP_ID
 HAVING count(*) < 4
 ORDER BY AVG_SALARY;
 
--- Query 6: Similar to 4B but instead of department ID use department name. 
--- Retrieve a list of employees ordered by department name, and within each 
+-- Query 6: Similar to 4B but instead of department ID use department name. Retrieve a list of employees ordered by department name, and within each 
 -- department ordered alphabetically in descending order by last name.
 
 SELECT D.DEP_NAME , E.F_NAME, E.L_NAME
